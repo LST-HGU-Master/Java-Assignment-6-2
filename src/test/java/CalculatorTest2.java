7ef435cd4ebaf22582bbd3059a24a8ccf5dd8ec2
YoshiHirat <50605381+YoshiHirat@users.noreply.github.com>
2022-05-23 21:17:40 +0900

    Update CalculatorTest2.java

--- src/test/java/CalculatorTest2.java
+++ src/test/java/CalculatorTest2.java
@@ -3,27 +3,29 @@ import org.junit.jupiter.api.Test;
 import java.io.*;
 
 public class CalculatorTest2 {
+    String pkgMthdName = "logicパッケージ内のCalculatorの";
+
     @Test
     public void testTime()
     {
         // assertion
-        assertEquals(2,  logic.Calculator.time(1 , 2), "積の計算結果が間違っています!");
-        assertEquals(36, logic.Calculator.time(4 , 9), "積の計算結果が間違っています!");
+        assertEquals(2,  logic.Calculator.time(1 , 2), pkgMthdName+"積の計算結果が間違っています!");
+        assertEquals(36, logic.Calculator.time(4 , 9), pkgMthdName+"積の計算結果が間違っています!");
     }
 
     @Test
     public void testDivide()
     {
         // assertion
-        assertEquals(0.5, logic.Calculator.divide( 1 , 2), 0.0001f, "商の計算結果が間違っています!");
-        assertEquals(5.0, logic.Calculator.divide(15 , 3), 0.0001f, "商の計算結果が間違っています!");
+        assertEquals(0.5, logic.Calculator.divide( 1 , 2), 0.0001f, pkgMthdName+"商の計算結果が間違っています!");
+        assertEquals(5.0, logic.Calculator.divide(15 , 3), 0.0001f, pkgMthdName+"商の計算結果が間違っています!");
     }
 
     @Test
     public void testDivideZero()
     {
         // assertion
-        assertEquals(0, logic.Calculator.divide( 1 , 0), 0.0001f, "商の計算結果が指示と異なります!");
-        assertEquals(0, logic.Calculator.divide(26 , 0), 0.0001f, "商の計算結果が指示と異なります!");
+        assertEquals(0, logic.Calculator.divide( 1 , 0), 0.0001f, pkgMthdName+"商の計算結果が指示と異なります!");
+        assertEquals(0, logic.Calculator.divide(26 , 0), 0.0001f, pkgMthdName+"商の計算結果が指示と異なります!");
     }
 }
